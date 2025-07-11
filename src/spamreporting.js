95a34eea2fe669484823e7d401c6f871cd235fdf
Jakob <60408771+J4K0B06@users.noreply.github.com>
2025-07-11 20:32:23 +0200

    Update spamreporting.js

--- src/spamreporting.js
+++ src/spamreporting.js
@@ -20,11 +20,7 @@ function onSpamReport(event) {
 
       // Run additional processing operations here.
 
-      /**
-       * Signals that the spam-reporting event has completed processing and shows a post-processing dialog to the user.
-       * If an error occurs while the message is being processed, the `onErrorDeleteItem` property determines whether the message will be deleted.
-       */
-      const event = asyncResult.asyncContext;
+      // Signals that the spam-reporting event has completed processing and shows a post-processing dialog to the user.
       event.completed({
         onErrorDeleteItem: false,
         moveItemTo: Office.MailboxEnums.MoveSpamItemTo.None,
